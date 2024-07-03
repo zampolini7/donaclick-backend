abf79dd3f951777ae47fdb7ef63db1ec3638b2a8
git <zampolini7@gmail.com>
2024-07-03 11:28:48 -0300

    Primer carga de la app

---
+++ donaclick-backend/src/app.module.ts
@@ -0,0 +1,10 @@
+import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ProductsModule } from './products/products.module';
+
+@Module({
+  imports: [MongooseModule.forRoot(process.env.MONGO_DB_TEST), ProductsModule],
+  controllers: [],
+  providers: [],
+})
+export class AppModule {}
